5de6ae565de02f3236a26cb5681d23f706fc4445
Shalvi19 <shalviharsh@gmail.com>
2020-06-24 01:26:23 +0530

    Finished matching with Euclidean distance

--- face_training.py
+++ face_training.py
@@ -14,61 +14,58 @@ The extracted features will then be used while classification.
 
 import cv2
 import dlib
-import face_recognition
+import face_recognition as freg
 import os
 import numpy as np
 import pickle
 
-print("Imports done !!!")
-folder_path = './dataset'    #100 Celeb dataset. 15 images per celeb
+def main():
 
-names = os.listdir(folder_path)
-total = 0
+    folder_path = 'Query_dataset'    #100 Celeb dataset. 15 images per celeb
 
-knownEmbeddings = []
-knownNames = []
-knownPath = []
+    names = os.listdir(folder_path)
+    total = 0
 
-detector = dlib.get_frontal_face_detector()  #Dlib Face detector
-print("detector loaded !!")
+    knownEmbeddings = []
+    knownNames = []
+    knownPath = []
 
+    detector = dlib.get_frontal_face_detector()  #Dlib Face detector
 
-for name in names:    #iterating over the folders in dataset - folders named with celeb names
-    print(name)
-    img_list = os.listdir(folder_path+'/'+name)
-    
-    print((img_list))
+    for name in names:    #iterating over the folders in dataset - folders named with celeb names
+        print(name)
+        img_list = os.listdir(folder_path+'/'+name)
 
-    cnt = 0
-    for img_path in img_list:
-        
-        pixels = cv2.imread(folder_path+'/'+name+'/'+img_path)
-        if pixels is None:
-            continue
-        if pixels.shape[0]<640 and pixels.shape[1]<480:    #discard face if its size is less than a particular value
-            continue
-        
-        rgb = cv2.cvtColor(pixels, cv2.COLOR_BGR2RGB)
-        boxes = face_recognition.face_locations(rgb)
-        
-        if len(boxes)==1: #consider only one face per image. If multiple faces present in the single image, discard them
-            cnt += 1
-            # compute the facial embedding for the face
-            encodings = face_recognition.face_encodings(rgb, boxes,num_jitters=1)
+        print((img_list))
 
-            knownEmbeddings.append(encodings[0])
-            knownNames.append(name)
-            knownPath.append(folder_path+'/'+name+'/'+img_path)
-        if cnt == 15:
-            break
-    print(name,cnt)
-data = {"embeddings": knownEmbeddings, "names": knownNames,"paths":knownPath}
+        cnt = 0
+        for img_path in img_list:
 
-#write the dictionary in the pickle file
-f = open('celeb_embedding.pickle', "wb")
-f.write(pickle.dumps(data))
-f.close()
+            pixels = cv2.imread(folder_path+'/'+name+'/'+img_path)
+            if pixels is None:
+               continue
+            if pixels.shape[0]<640 and pixels.shape[1]<480:    #discard face if its size is less than a particular value
+                continue
+
+            rgb = cv2.cvtColor(pixels, cv2.COLOR_BGR2RGB)
+            boxes = freg.face_locations(rgb)
+
+            if len(boxes)==1:
+                cnt += 1
+                encodings = freg.face_encodings(rgb, boxes,num_jitters=1)
+
+                knownEmbeddings.append(encodings[0])
+                knownNames.append(name)
+                knownPath.append(folder_path+'/'+name+'/'+img_path)
+            if cnt == 15:
+                break
+        print(name,cnt)
+    data = {"embeddings": knownEmbeddings, "names": knownNames,"paths":knownPath}
+
+    #write the dictionary in the pickle file
+    f = open('Query_embedding.pickle', "wb")
+    f.write(pickle.dumps(data))
+    f.close()
 
 
-
-
+main()
